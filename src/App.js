1e9b341a80174c927d7944e6c5f22040953477fe
crimsoftit <crimsoftit@gmail.com>
2022-02-11 19:58:39 +0300

    2. building out the frontend

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
 import 'regenerator-runtime/runtime';
 import React,{useState, Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { login, logout } from './utils';
 import './global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import getConfig from './config';
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
 
-export default function App() {
-	return (
-	<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-		<Container>
-			<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mx-auto">
-					<Nav.Link href="#features">Features</Nav.Link>
-					<Nav.Link href="#pricing">Pricing</Nav.Link>
-					<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-					<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-					</NavDropdown>
-				</Nav>
+export default function BasicExample() {
+  return (
+		<Router>
+			<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+			<Container>
+				<Navbar.Brand href='/'>Home Page</Navbar.Brand>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+				<Nav className='mx-auto'></Nav>
 				<Nav>
-					<Nav.Link>New Poll</Nav.Link>
-					<Nav.Link>
-					Sign in
+					<Nav.Link href='/NewPoll'>New Poll</Nav.Link>
+					<Nav.Link onClick={window.accountId === "" ? login : logout}>
+					{window.accountId === "" ? "Login" : window.accountId}
 					</Nav.Link>
 				</Nav>
-			</Navbar.Collapse>
-		</Container>
-	</Navbar>);
+				</Navbar.Collapse>
+			</Container>
+			</Navbar>
+			<Switch>
+				<Route>
+					<Home />
+				</Route>
+				<Route>
+					<NewPoll />
+				</Route>
+				<Route>
+					<PollingStation />
+				</Route>
+			</Switch>
+		</Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function NewPoll() {
+  return (
+    <div>
+      <h2>NewPoll</h2>
+    </div>
+  );
 }
---
+++ src/Components/NewPoll.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class NewPoll extends React.Component {
+	render() {
+	  return (
+		<h1>NewPoll</h1>
+	  )
+	}
+}
